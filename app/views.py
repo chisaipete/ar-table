2aafc1f0aa1b298ed2b3f011e860dbefc9cdc251
Connor Petersen <cmpete@gmail.com>
2017-05-17 10:23:04 -0700

    final forms fix

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm
-from .forms import LoginForm
+# from .forms import LoginForm
 from .models import User
 from .oauth import OAuthSignIn
 

a6403e86828ecdb37801a4fd67b30c5992182df9
Connor Petersen <cmpete@gmail.com>
2017-05-17 00:45:15 -0700

    Oauth working with Twitter, fantastic!  Basic user db created, and proved authentication system is working

--- app/views.py
+++ app/views.py
@@ -1,14 +1,56 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
 from .forms import LoginForm
+from .models import User
+from .oauth import OAuthSignIn
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = None
+    user = g.user
     return render_template("index.html", title='Home', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    return render_template("login.html", title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="{}", remember_me={}'.format(form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template("login.html", title='Sign In', form=form, providers=app.config['OAUTH_CREDENTIALS'].keys())
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
--- config.py
+++ config.py
@@ -1,2 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'i-love-magic-beans'
+OAUTH_CREDENTIALS = {
+    'twitter': {
+        'id': '58bWCkDctbLCxYtqVTWycSVqv',
+        'secret': 'nt8dondH3nb1EFtt8hIntMkpUEfDnNVBZQvfJzSU50uny5ZYWb',
+    },
+}
